d7a40dc54f17ddf929d7c25bf5805dbedd690dc5
nicola camp <nicola.camp89@yahoo.com>
2023-07-25 18:58:35 +0100

    CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,43 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
   end
 
-  def created
-    task = Task.new(task_params)
-    task.save
-    redirect_to task_path(task)
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
   end
-end
 
-private
+  def edit
+  end
 
-def task_params
-  params.require(task).permit(:title, :details)
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
